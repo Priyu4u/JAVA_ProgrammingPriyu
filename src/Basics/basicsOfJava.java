a1f5b6b319ab19ef8d5adb75989b83b22055ea5c
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-24 22:05:01 +0530

    basics

--- src/Basics/basicsOfJava.java
+++ src/Basics/basicsOfJava.java
@@ -3,5 +3,8 @@ package Basics;
 public class basicsOfJava {
     public static void main(String[] args) {
         System.out.println("Hello This is Priyanshu");
+        System.out.println("I am learning JAVA programming language");
+        System.out.println(4+2);
+        System.out.println(5*6);   // used to print something on screen
     }
 }
