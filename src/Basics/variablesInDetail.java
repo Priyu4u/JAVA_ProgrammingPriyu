d772b0f0a93394dfd3fd5cb9890ebfa01fa18607
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-24 22:09:08 +0530

    variables in detail

---
+++ src/Basics/variablesInDetail.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class variablesInDetail {
+    public static void main(String[] args) {
+        int x = 80, y = 50, z = 10;
+        System.out.print("The value of x is ");
+        System.out.println(x);
+        System.out.println("The value of y is "+y);
+        z = z + 10;
+        System.out.println("The value of z is "+z);
+        System.out.println(x+y+z);
+        System.out.println("The sum of values of x, y and z is "+x+y+z);
+        System.out.println(""+x+y+z);
+        System.out.println(x+"y"+z);
+        System.out.println(x+""+y+z);
+        System.out.println(x+y+""+z);
+        System.out.println(x+y+z+"");
+    }
+}
