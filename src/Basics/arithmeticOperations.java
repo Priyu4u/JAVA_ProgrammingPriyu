9320ecdeffc9d837ff40f96951f809c65f87ed1d
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-24 22:08:37 +0530

    arithmetic operators

---
+++ src/Basics/arithmeticOperations.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class arithmeticOperations {
+    public static void main(String[] args) {
+        int x = 25 ;
+        int y = 20 ;
+        // arithmetic operators ->  + , - , * , /
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println((float)x/y);
+    }
+}
