367ededc2422dfcefe62c3996c719b7ab6731e0c
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-24 22:08:54 +0530

    int data type

---
+++ src/Basics/variables_int.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class variables_int {
+    public static void main(String[] args) {
+        int x ;   // int data type stores 4 bytes of memory
+        x = 50 ;
+        System.out.println(x);
+        x = 302;
+        System.out.println(x);
+        System.out.println(x+25);
+        x = x + 2 ;
+        System.out.println(x);
+    }
+}
