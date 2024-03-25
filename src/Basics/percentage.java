63addd5741b79737d688d5e5f58d39ef84205352
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-25 21:32:58 +0530

    percentage calculations

---
+++ src/Basics/percentage.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class percentage {
+    public static void main(String[] args) {
+        // calculating percentage of 5 subjects
+        double x1 = 90 ;    // english
+        double x2 = 91;     // hindi
+        double x3 = 93;     // maths
+        double x4 = 94;     // science
+        double x5 = 97;     // social science
+        double percent = (x1+x2+x3+x4+x5) / 5;
+        System.out.println(percent);
+    }
+}
