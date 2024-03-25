6d5c1f4538b99935cc2b3db4685ef0bc6bfa094b
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-25 21:34:49 +0530

    volume of sphere

---
+++ src/Basics/volumeOfSphere.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class volumeOfSphere {
+    public static void main(String[] args) {
+        // WAP to print volume of a sphere with given radius
+        double rad = 4.22;
+        double pi = 3.1415;
+        double vol = (4 * pi * rad * rad * rad) / 3;
+        System.out.println(vol);
+
+
+    }
+}
