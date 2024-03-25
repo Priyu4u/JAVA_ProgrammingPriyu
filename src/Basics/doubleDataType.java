f9aaa3cf4bea9da7748eff762c3f2d0d3d4e82ca
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-25 18:06:22 +0530

    double data type

---
+++ src/Basics/doubleDataType.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class doubleDataType {
+    public static void main(String[] args) {
+        double a = 2.25 ;
+        System.out.println(a);
+        double x = 5;
+        double y = 2;
+        double z = x / y ;
+        System.out.println(z);
+        double z2 = 5.0 / 2 ;
+        System.out.println(z2);
+    }
+}
