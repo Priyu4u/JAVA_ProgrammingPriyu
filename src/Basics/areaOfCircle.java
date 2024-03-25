19a984ebb09ebf001ed5ab7fced66641e3fb8a4c
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-25 18:06:09 +0530

    area of a circle

---
+++ src/Basics/areaOfCircle.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class areaOfCircle {
+    public static void main(String[] args) {
+        // WAP to calculate the area of a circle
+        double rad = 10;
+        double pi = 3.1415;
+        double area  = pi * rad * rad ;
+        System.out.println("The area of circle is "+area);
+    }
+}
