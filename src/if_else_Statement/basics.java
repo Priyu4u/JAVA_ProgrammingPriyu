0a347612af5aa8a72b38d1bc5a333b38bc52ba2c
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-26 21:46:37 +0530

    basics

--- src/if_else_Statement/basics.java
+++ src/if_else_Statement/basics.java
@@ -1,7 +1,18 @@
 package if_else_Statement;
-
+import java.util.Scanner;
 public class basics {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in) ;
+
+        // if-else also known as conditional statements are used to for decision-making in programming
+        /*
+        SYNTAX
+        if(condition){
+            // code
+        }
+        else{
+            // code
+        }
+         */
     }
 }
