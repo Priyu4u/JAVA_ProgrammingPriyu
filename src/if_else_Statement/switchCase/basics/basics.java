8744b3e1b37e52006e091129c6de653b8fbb7e97
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-30 11:22:45 +0530

    if else statement

---
+++ src/if_else_Statement/switchCase/basics/basics.java
@@ -0,0 +1,23 @@
+package if_else_Statement.switchCase.basics;
+import java.util.Scanner;
+public class basics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //SWITCH CASE
+        // SYNTAX
+        /*
+
+        switch(expression){
+            case a:
+                // code
+                break;
+            case b:
+                // code
+                break;
+            default:
+                // code
+        }
+
+         */
+    }
+}
