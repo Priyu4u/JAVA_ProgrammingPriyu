e0820bfa40bd3791fa18a79fa2a3b46aa5da1316
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-03-30 11:23:34 +0530

    operators notes

---
+++ src/javaOperators/basics.java
@@ -0,0 +1,68 @@
+package javaOperators;
+import java.util.Scanner;
+public class basics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Java Operators
+        /*
+
+        Arithmetic Operators
+        ->    +  -  *  /  %(modulo)  ++(increment)   --(decrement)
+
+        Relational Operators{generally used for comparison}
+        ->    ==(is equal to)
+        ->    !=(is not equal to)
+        ->    < (greater than)    and    > (less than)
+        ->    <= (greater than or equals to)    and    >= (less than or equal to)
+
+        Logical Operators
+        ->      &&(logical AND)
+        ->      ||(logical OR)
+        ->      !(logical NOT)
+
+        Assignment Operators
+        ->      =   +=  -=  /=  %=
+
+        Unary Operators{operators that required only one operand}
+        ->      +(unary plus)
+        ->      -(unary minus)
+        ->      ++(increment operator)
+                    post-increment
+                    pre-increment
+        ->      --(decrement operator)
+                    post-decrement
+                    pre-decrement
+        ->      ! (logical complement operator)
+
+        Bitwise Operators{checks each binary bit / operates on bit level}
+        ->      | (Bitwise OR)
+                    if any one bit is 1, then output will be 1
+
+        ->      & (Bitwise AND)
+                    if all the bits are 1, then only output will be 1
+
+        ->      ^ (Bitwise exclusive OR or XOR)
+                    if two bits are same then the output will be 0
+                    if two bits are different then the output will be 1
+
+        ->      ~ (Bitwise Complement or NOT)
+                    inverts all the bits
+
+        ->      << (left shift)             a << b = a * 2^b
+                    shifts bits to left
+        ->      >> (right shift)
+                    shifts bits to right
+
+         */
+
+        /*
+        JAVA OPERATORS PRECEDENCE AND ASSOCIATIVITY
+
+        *PRECEDENCE -> defines the order of evaluation/power of operator
+        *ASSOCIATIVITY -> order of evaluation in terms of left to right or right to left
+
+
+         */
+
+    }
+}
