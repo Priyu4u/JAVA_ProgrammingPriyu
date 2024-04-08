09378b68d5d6e6f53f248c0b026a5f9dac106947
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-04-08 21:30:39 +0530

    loops basics

---
+++ src/Loops/basic.java
@@ -0,0 +1,20 @@
+package Loops;
+import java.util.Scanner;
+public class basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // loops are used for task which repeats too much
+        // types
+        // while loop
+        // for loop
+        // do while loop
+
+        // syntax
+        // while
+        // while(condition){
+            // code
+        // }
+
+    }
+}
