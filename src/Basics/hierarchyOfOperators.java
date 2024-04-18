24df6383c5dac7b0e65456af75b7ae9dbaac781d
Priyanshu Gupta <gpriyanshuu4u@gmail.com>
2024-04-18 18:18:32 +0530

    hierarchy of operators

---
+++ src/Basics/hierarchyOfOperators.java
@@ -0,0 +1,10 @@
+package Basics;
+import java.util.Scanner;
+public class hierarchyOfOperators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // hierarchy of operators
+        int i = 2 * 3 / 4;
+        System.out.println(i);
+    }
+}
